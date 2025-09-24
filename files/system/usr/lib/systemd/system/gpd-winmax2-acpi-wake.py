07116355f9c7cce6c8b6a580df495e6780245da0
XiaoLongBaoLab <ben.mixian@pm.me>
2025-09-24 15:12:22 +0700

    Update gpd-winmax2-acpi-wake.py

--- files/system/usr/lib/systemd/system/gpd-winmax2-acpi-wake.py
+++ files/system/usr/lib/systemd/system/gpd-winmax2-acpi-wake.py
@@ -2,7 +2,7 @@
 Description=Configure ACPI Wake devices for GPD WIN MAX 2
 
 [Service]
-ExecStart=/usr/local/bin/gpd-winmax2-acpi-wake.py
+ExecStart=/usr/libexec/gpd-winmax2-acpi-wake.py
 
 [Install]
 WantedBy=multi-user.target
